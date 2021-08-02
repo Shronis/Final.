afdfc5c3118b3bf3fb686d9d7a94f0f5000d3de0
Shronis <lmincorvaia@gmail.com>
2021-08-02 13:17:44 -0400

    Task: Part 11 complete

--- app/config.py
+++ app/config.py
@@ -1,29 +1,16 @@
-"""Flask app configuration."""
-from os import environ, path
-from dotenv import load_dotenv
-
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+"""App configuration."""
+from os import environ
+import redis
 
 
 class Config:
-    """Set Flask configuration from environment variables."""
+    """Set Flask configuration vars from .env file."""
 
-    FLASK_APP = 'wsgi.py'
-    FLASK_ENV = environ.get('FLASK_ENV')
+    # General Config
     SECRET_KEY = environ.get('SECRET_KEY')
+    FLASK_APP = environ.get('FLASK_APP')
+    FLASK_ENV = environ.get('FLASK_ENV')
 
-    # Flask-Assets
-    LESS_BIN = environ.get('LESS_BIN')
-    ASSETS_DEBUG = environ.get('ASSETS_DEBUG')
-    LESS_RUN_IN_DEBUG = environ.get('LESS_RUN_IN_DEBUG')
-
-    # Static Assets
-    STATIC_FOLDER = 'static'
-    TEMPLATES_FOLDER = 'templates'
-    COMPRESSOR_DEBUG = environ.get('COMPRESSOR_DEBUG')
-
-    # Flask-SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_ECHO = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Flask-Session
+    SESSION_TYPE = environ.get('SESSION_TYPE')
+    SESSION_REDIS = redis.from_url(environ.get('SESSION_REDIS'))
