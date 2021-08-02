893330a9c69e38ba93557a5ded4a11799b06be6d
Shronis <lmincorvaia@gmail.com>
2021-08-02 13:04:22 -0400

    Task: Part 8 complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,25 @@
-"""Initialize Flask app."""
+"""Initialize app."""
 from flask import Flask
+from flask_assets import Environment
+from .assets import compile_assets
+
+assets = Environment()
 
 
 def create_app():
-    """Create Flask application."""
+    """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
+    # Initialize plugins
+    assets.init_app(app)
+
     with app.app_context():
         # Import parts of our application
-        from .profile import routes
-        from .home import routes
-        from .products import routes
-
-        # Register Blueprints
-        app.register_blueprint(profile.account_bp)
-        app.register_blueprint(home.home_bp)
-        app.register_blueprint(products.product_bp)
+        from .admin import routes
+        from .main importn routes
+        app.register_blueprint(admin_routes.admin_bp)
+        app.register_blueprint(main_routes.main_bp)
+        compile_assets(assets)
 
         return app
