e03984b70aa2f867f38aa4f196321a288224cc2e
Shronis <lmincorvaia@gmail.com>
2021-08-02 13:11:40 -0400

    Task: Part 10 complete

--- app/forms.py
+++ app/forms.py
@@ -1,81 +1,58 @@
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import (
-    StringField,
-    TextAreaField,
-    SubmitField,
-    PasswordField,
-    DateField,
-    SelectField
-)
+"""Sign-up & log-in forms."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import (
     DataRequired,
     Email,
     EqualTo,
     Length,
-    URL
+    Optional
 )
 
-class ContactForm(FlaskForm):
-    """Contact form."""
+
+class SignupForm(FlaskForm):
+    """User Sign-up Form."""
     name = StringField(
         'Name',
-        [DataRequired()]
+        validators=[DataRequired()]
     )
     email = StringField(
         'Email',
-        [
-            #Email(message=('Not a valid email address.')),
+        validators=[
+            Length(min=6),
+            Email(message='Enter a valid email.'),
             DataRequired()
         ]
     )
-    body = TextField(
-        'Message',
-        [
+    password = PasswordField(
+        'Password',
+        validators=[
             DataRequired(),
-            Length(min=4,
-            message=('Your message is too short.'))
+            Length(min=6, message='Select a stronger password.')
         ]
     )
-    #recaptcha = RecaptchaField()
-    submit = SubmitField('Submit')
+    confirm = PasswordField(
+        'Confirm Your Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password', message='Passwords must match.')
+        ]
+    )
+    website = StringField(
+        'Website',
+        validators=[Optional()]
+    )
+    submit = SubmitField('Register')
 
-    class SignupForm(FlaskForm):
-        """Sign up for a user account."""
-        email = StringField(
-            'Email',
-            [
-                Email(message='Not a valid email address.'),
-                DataRequired()
-            ]
-        )
-        password = PasswordField(
-            'Password',
-            [
-                DataRequired(message="Please enter a password."),
-            ]
-        )
-        confirmPassword = PasswordField(
-            'Repeat Password',
-            [
-                EqualTo(password, message='Passwords must match.')
-            ]
-        )
-        title = SelectField(
-            'Title',
-            [DataRequired()],
-            choices=[
-                ('Farmer', 'farmer'),
-                ('Corrupt Politician', 'politician'),
-                ('No-nonsense City Cop', 'cop'),
-                ('Professional Rocket League Player', 'rocket'),
-                ('Lonely Guy At A Diner', 'lonely'),
-                ('Pokemon Trainer', 'pokemon')
-            ]
-        )
-        website = StringField(
-            'Website',
-            validators=[URL()]
-        )
-        birthday = DateField('Your Birthday')
-        recaptcha = RecaptchaField()
-        submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    """User Log-in Form."""
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Email(message='Enter a valid email.')
+        ]
+    )
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
